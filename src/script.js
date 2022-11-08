60545d4ecab5e9d5c58de0b0d389c72f167eb52c
Kevin <kevin.castro.chavez0@gmail.com>
2022-11-08 10:17:12 -0700

    learned how to scale objects

--- src/script.js
+++ src/script.js
@@ -13,18 +13,24 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// POSITION
+// mesh.position.normalize(); // Results in 1
 // mesh.position.x = 0.7;
 // mesh.position.y = -0.6;
 // mesh.position.z = 1;
 // Same thing as above
 mesh.position.set(0.7, -0.6, 1);
 
-mesh.position.normalize(); // Results in 1
+// SCALE
+// mesh.scale.x = 2;
+// mesh.scale.y = 0.5;
+// mesh.scale.z = 0.5;
+mesh.scale.set(2, 0.5, 0.5); // Same thing as above
 
 // Distance between the center of the scene and the object position
 console.log(mesh.position.length());
-
-scene.add(mesh);
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper(2); // Creates the lines. Parameter is how long the lines should be
